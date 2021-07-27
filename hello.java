c77abf96cdb7745f631b51344fd589038f211658
unknown <sharma.razev012@gmail.com>
2021-07-27 18:18:14 -0500

    modf by dev2

--- hello.java
+++ hello.java
@@ -1,9 +1,9 @@
-class hello #from dev1
+class hello #from dev2
 {
 
 
-	public static void main()  # from dev1
+	public static void main()  # from dev2
 	{
-		system.out.println("hello from dev1"); 
+		system.out.println("hello"); 
 	}
-}
+}#from dev2
